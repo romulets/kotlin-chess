1334fa369257e104535b0b95933c36abd625bad6
Rômulo M. de Farias <romulodefarias@gmail.com>
2019-03-24 06:10:09 -0300

    Add working test

---
+++ src/test/kotlin/com/romulo/chess/GameBoardTest.kt
@@ -0,0 +1,13 @@
+package com.romulo.chess
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class GameBoardTest {
+
+    @Test
+    fun testInitialState() {
+        assertEquals(1, 1);
+    }
+
+}
