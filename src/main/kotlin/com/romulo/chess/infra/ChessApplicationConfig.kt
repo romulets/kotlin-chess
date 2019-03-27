cbb91ed906b967cf5c48daa70ee7fcebbf94c5b8
Rômulo M. de Farias <romulodefarias@gmail.com>
2019-03-26 21:31:59 -0300

    Add cors configuration

---
+++ src/main/kotlin/com/romulo/chess/infra/ChessApplicationConfig.kt
@@ -0,0 +1,19 @@
+package com.romulo.chess.infra
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.romulo.chess")
+class ChessApplicationConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
