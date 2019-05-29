bf57b6fcf665446cdabc3e3a769d3eef53bfc96c
Rômulo M. de Farias <romulodefarias@gmail.com>
2019-05-29 18:34:09 -0300

    Add move to queen

--- src/main/kotlin/com/romulo/chess/domain/piece/Queen.kt
+++ src/main/kotlin/com/romulo/chess/domain/piece/Queen.kt
@@ -5,11 +5,17 @@ import com.romulo.chess.domain.Position
 
 class Queen(
     override val color: Color,
-    override val position: Position
+    override var position: Position
 ) : Piece {
 
     override fun moveTo(position: Position, pieceAt: (position: Position) -> Piece?): Boolean {
-        TODO("not implemented")
+        if (!possiblePositions(pieceAt).contains(position)) {
+            return false
+        }
+
+        this.position = position
+
+        return true
     }
 
     override fun possiblePositions(pieceAt: (position: Position) -> Piece?): List<Position> {
--- src/test/kotlin/com/romulo/chess/domain/piece/QueenTest.kt
+++ src/test/kotlin/com/romulo/chess/domain/piece/QueenTest.kt
@@ -2,7 +2,9 @@ package com.romulo.chess.domain.piece
 
 import com.romulo.chess.domain.Color
 import com.romulo.chess.domain.Position
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class QueenTest {
@@ -107,5 +109,84 @@ class QueenTest {
         assertPossiblePositionContains(possiblePositions, 5, 'f')
         assertPossiblePositionContains(possiblePositions, 5, 'd')
         assertEquals(8, possiblePositions.size)
+    }
+
+    @Test
+    fun testMoveUp() {
+        val queen = Queen(Color.WHITE, Position(4, 'e'))
+        assertTrue(queen.moveTo(Position(1, 'e')))
+        assertEquals(queen.position, Position(1, 'e'))
+    }
+
+    @Test
+    fun testMoveDown() {
+        val queen = Queen(Color.WHITE, Position(4, 'e'))
+        assertTrue(queen.moveTo(Position(8, 'e')))
+        assertEquals(queen.position, Position(8, 'e'))
+    }
+
+    @Test
+    fun testMoveLeft() {
+        val queen = Queen(Color.WHITE, Position(4, 'e'))
+        assertTrue(queen.moveTo(Position(4, 'a')))
+        assertEquals(queen.position, Position(4, 'a'))
+    }
+
+    @Test
+    fun testMoveRight() {
+        val queen = Queen(Color.WHITE, Position(4, 'e'))
+        assertTrue(queen.moveTo(Position(4, 'h')))
+        assertEquals(queen.position, Position(4, 'h'))
+    }
+
+    @Test
+    fun testMoveRightDown() {
+        val queen = Queen(Color.WHITE, Position(5, 'e'))
+        assertTrue(queen.moveTo(Position(2, 'h')))
+        assertEquals(queen.position, Position(2, 'h'))
+    }
+
+    @Test
+    fun testMoveLeftDown() {
+        val queen = Queen(Color.WHITE, Position(5, 'e'))
+        assertTrue(queen.moveTo(Position(1, 'a')))
+        assertEquals(queen.position, Position(1, 'a'))
+    }
+
+    @Test
+    fun testMoveRightUp() {
+        val queen = Queen(Color.WHITE, Position(5, 'e'))
+        assertTrue(queen.moveTo(Position(8, 'h')))
+        assertEquals(queen.position, Position(8, 'h'))
+    }
+
+    @Test
+    fun testMoveLeftUp() {
+        val queen = Queen(Color.WHITE, Position(5, 'e'))
+        assertTrue(queen.moveTo(Position(8, 'b')))
+        assertEquals(queen.position, Position(8, 'b'))
+    }
+
+    @Test
+    fun testNotMoveWeirdPosition() {
+        val queen = Queen(Color.WHITE, Position(4, 'e'))
+        Assertions.assertFalse(queen.moveTo(Position(7, 'a')))
+        assertEquals(queen.position, Position(4, 'e'))
+    }
+
+    @Test
+    fun testEat() {
+        val queen = Queen(Color.WHITE, Position(4, 'e'))
+        val to = Position(3, 'e')
+        assertTrue(queen.moveTo(to, returnPieceIf(to::sameThat, Color.BLACK)))
+        assertEquals(queen.position, to)
+    }
+
+    @Test
+    fun testNotEatFriend() {
+        val queen = Queen(Color.WHITE, Position(4, 'e'))
+        val to = Position(3, 'e')
+        Assertions.assertFalse(queen.moveTo(to, returnPieceIf(to::sameThat, Color.WHITE)))
+        assertEquals(queen.position, Position(4, 'e'))
     }
 }
